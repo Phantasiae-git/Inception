409a093bd7cbf830dfded197906bb3f073e4af70
Phantasiae <phantasiae.flr@gmail.com>
2025-05-23 19:48:12 +0100

    ssl/tls certificate, domain, volume directory

--- src/requirements/wordpress/tools/wp.sh
+++ src/requirements/wordpress/tools/wp.sh
@@ -5,7 +5,7 @@ sleep 8
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb-c --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=rfontes- --admin_password=ya --admin_email=raquel.e5317@gmail.com --allow-root
+./wp-cli.phar core install --url=rfontes-.42.fr --title=inception --admin_user=rfontes- --admin_password=ya --admin_email=raquel.e5317@gmail.com --allow-root
 ./wp-cli.phar user create user user@ya.com --role=subscriber --user_pass=yauser --allow-root
 
 php-fpm7.4 -F
